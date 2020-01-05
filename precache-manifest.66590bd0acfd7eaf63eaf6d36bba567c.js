00e15defa4e401769bb047e7c4ba3d94da16005b
dracuten1 <tuyennguyenleminh@gmail.com>
2020-01-05 17:22:52 +0700

    Updates

---
+++ precache-manifest.66590bd0acfd7eaf63eaf6d36bba567c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c36a728bde2a3a05d6b9de7f5d9861",
+    "url": "/tkgd-admin/index.html"
+  },
+  {
+    "revision": "4e43f6c046cd604d2d95",
+    "url": "/tkgd-admin/static/css/2.7ef3a9eb.chunk.css"
+  },
+  {
+    "revision": "4e8f63d4ab6e149f006e",
+    "url": "/tkgd-admin/static/css/main.6a73e6e3.chunk.css"
+  },
+  {
+    "revision": "4e43f6c046cd604d2d95",
+    "url": "/tkgd-admin/static/js/2.8168901f.chunk.js"
+  },
+  {
+    "revision": "abc45bf3f87fc006a0019211547d064d",
+    "url": "/tkgd-admin/static/js/2.8168901f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4e8f63d4ab6e149f006e",
+    "url": "/tkgd-admin/static/js/main.3880307b.chunk.js"
+  },
+  {
+    "revision": "93325f35e9c617d1188f",
+    "url": "/tkgd-admin/static/js/runtime-main.0d358341.js"
+  },
+  {
+    "revision": "61207a6a2d6370f530232d8bfb22161f",
+    "url": "/tkgd-admin/static/media/bg.61207a6a.svg"
+  },
+  {
+    "revision": "abe4a318dfd1daed6d59f969027b0b53",
+    "url": "/tkgd-admin/static/media/bg.abe4a318.png"
+  }
+]);
